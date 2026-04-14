5ff09339a2840ff6afe8c09093a1cd88c5dbf75d
suxql <20226410@s.ubaguio.edu>
2026-04-14 13:53:24 +0800

    Refactor background setup in assembly code for clarity and consistency

--- G5.asm
+++ G5.asm
@@ -1,23 +1,29 @@
 .model small
 .stack 100h
 .data
+    msg db "test$"
 .code
     mov ax, @data
     mov ds, ax
-
+    
     ;set video mode
     mov ah, 00h
     mov al, 3 
     int 10h
 
-    ;bg
-    mov ah, 6 
+    ;outer most bg
+    mov ah, 6 ;scroll up
     mov al, 00h
-    mov bh, 0010h
+
+    mov bh, 11h ;blue bg blue text
     mov cx, 0000h ;row 1 col 1
     mov dx, 184fh ;row 25 col 80
     int 10h
-    
+
+    mov ah, 9
+    lea dx, msg
+    int 21h
+
     mov ah, 4ch
     int 21h
 END
