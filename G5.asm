2dc36c0f468bfed3532b8cefed5244a3db3d4085
suxql <20226410@s.ubaguio.edu>
2026-04-14 13:50:54 +0800

    Add background setup to video mode in assembly code

--- G5.asm
+++ G5.asm
@@ -9,6 +9,14 @@
     mov ah, 00h
     mov al, 3 
     int 10h
+
+    ;bg
+    mov ah, 6 
+    mov al, 00h
+    mov bh, 0010h
+    mov cx, 0000h ;row 1 col 1
+    mov dx, 184fh ;row 25 col 80
+    int 10h
     
     mov ah, 4ch
     int 21h
