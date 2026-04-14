5632bbf3e06e55d2b21bbd24962177e46b1e4cd0
suxql <20226410@s.ubaguio.edu>
2026-04-14 15:01:28 +0800

    Refactor background setup in assembly code for improved clarity and consistency

--- G5.asm
+++ G5.asm
@@ -16,15 +16,23 @@
     mov al, 00h
 
     mov bh, 11h ;blue bg blue text
-    mov cx, 0000h ;row 1 col 1
-    mov dx, 184fh ;row 25 col 80
+    mov cx, 0000h ;start row 0 col 0
+    mov dx, 184fh ;end row 24 col 79
     int 10h
 
     ;instructions and feedback box
     mov bh, 33h ;cyan bg cyan text
-    mov cx, 0104h ;row 1 col 1
-    mov dx, 174ah ;row 25 col 80
+    mov cx, 0104h ;start row 1 col 4 
+    mov dx, 174bh ;end row 23 col 75
     int 10h
+
+    ;grid area
+    mov bh, 11h ;blue bg blue text
+    mov cx, 0604h ;start row 5 col 4
+    mov dx, 124bh ;end row 19 col 75
+    int 10h
+
+
 
     mov ah, 9
     lea dx, msg
