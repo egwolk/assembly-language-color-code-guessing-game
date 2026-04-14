886c24346aac29f6ef882a889e8e843c9fbb2c1e
suxql <20226410@s.ubaguio.edu>
2026-04-14 13:59:17 +0800

    Add instructions and feedback box setup in assembly code

--- G5.asm
+++ G5.asm
@@ -20,6 +20,12 @@
     mov dx, 184fh ;row 25 col 80
     int 10h
 
+    ;instructions and feedback box
+    mov bh, 33h ;cyan bg cyan text
+    mov cx, 0104h ;row 1 col 1
+    mov dx, 174ah ;row 25 col 80
+    int 10h
+
     mov ah, 9
     lea dx, msg
     int 21h
